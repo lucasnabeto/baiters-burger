461daa8aa7b9192ee284f0b0cdc91eee83d4657e
Lucas Nabeto <lucas.nabeto@outlook.com>
2025-05-06 23:23:08 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/baitersburger/BaitersburgerApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.baitersburger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaitersburgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaitersburgerApplication.class, args);
+	}
+
+}
