1ecd9a5984a35fd5cd53f8462865fc72f729401f
Leonardo Kegel <leonardo.kegel@gmail.com>
2025-05-19 22:04:37 -0300

    fix: arruma o mapper

--- src/main/java/br/com/fiap/baitersburger/adapters/in/controller/dto/order/OrderRequestDTO.java
+++ src/main/java/br/com/fiap/baitersburger/adapters/in/controller/dto/order/OrderRequestDTO.java
@@ -1,11 +1,14 @@
 package br.com.fiap.baitersburger.adapters.in.controller.dto.order;
 
 import br.com.fiap.baitersburger.core.domain.model.Product;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class OrderRequestDTO {
+
+    @NotEmpty
     private List<Product> products;
 }
--- src/main/java/br/com/fiap/baitersburger/adapters/in/controller/mapper/OrderMapper.java
+++ src/main/java/br/com/fiap/baitersburger/adapters/in/controller/mapper/OrderMapper.java
@@ -2,7 +2,9 @@ package br.com.fiap.baitersburger.adapters.in.controller.mapper;
 
 import br.com.fiap.baitersburger.adapters.in.controller.dto.order.OrderRequestDTO;
 import br.com.fiap.baitersburger.core.domain.model.Order;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface OrderMapper {
     Order toOrder(OrderRequestDTO orderRequestDTO);
 }
