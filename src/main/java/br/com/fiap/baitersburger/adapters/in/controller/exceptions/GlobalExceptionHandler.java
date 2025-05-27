c60d60834581cb7154a3479080840e4f4982a2c2
Daniel Ferreira <daniel.ferreira.dev@gmail.com>
2025-05-26 22:22:31 -0300

    validation and treatment exception for cpf, email and name

--- src/main/java/br/com/fiap/baitersburger/adapters/in/controller/exceptions/GlobalExceptionHandler.java
+++ src/main/java/br/com/fiap/baitersburger/adapters/in/controller/exceptions/GlobalExceptionHandler.java
@@ -1,27 +1,53 @@
 package br.com.fiap.baitersburger.adapters.in.controller.exceptions;
 
 import br.com.fiap.baitersburger.core.exceptions.NotFoundException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleCustomerNotFound(NotFoundException ex) {
+    public ResponseEntity<Map<String, Object>> handleNotFound(NotFoundException ex) {
 
-        Map<String, Object> body = new HashMap<>();
-        body.put("timestamp", LocalDateTime.now());
-        body.put("status", HttpStatus.NOT_FOUND.value());
-        body.put("message", ex.getMessage());
+        Map<String, Object> body = fillBody(ex.getMessage(), HttpStatus.NOT_FOUND.value());
 
         // Retorna um erro 404 com o objeto de erro
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
+    }
+
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        
+        List<String> errorMessages = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+        
+       Map<String, Object> body = fillBody(errorMessages, HttpStatus.BAD_REQUEST.value());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+    }
+
+    private static Map<String, Object> fillBody(Object message, int status) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", status);
+        body.put("message", message);
+        return body;
     }
 }
